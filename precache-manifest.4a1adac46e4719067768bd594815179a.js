5e17add97769e1130f8addc059517388a9eb4de0
Belga-JS <Meherzi.belga@yahoo.com>
2019-11-18 15:27:49 +0100

    Updates

---
+++ precache-manifest.4a1adac46e4719067768bd594815179a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75aa9d30046c88474991bb35ad872f7",
+    "url": "/Credit-Card/index.html"
+  },
+  {
+    "revision": "8cdd22083950879339d3",
+    "url": "/Credit-Card/static/css/main.79431578.chunk.css"
+  },
+  {
+    "revision": "66011f513b77336dea57",
+    "url": "/Credit-Card/static/js/2.bfbc1e0e.chunk.js"
+  },
+  {
+    "revision": "8cdd22083950879339d3",
+    "url": "/Credit-Card/static/js/main.888a2ab7.chunk.js"
+  },
+  {
+    "revision": "70c533731d8dda0f7381",
+    "url": "/Credit-Card/static/js/runtime-main.ab941e0f.js"
+  }
+]);
